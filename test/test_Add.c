ee9bcf65fef3f29ac1acc6bbdb8c5b38d1960621
ChaosAD <frasker@hotmail.com>
2019-05-27 12:46:57 +0800

    Implemented and tested the add()

--- test/test_Add.c
+++ test/test_Add.c
@@ -1,15 +1,16 @@
 #include "unity.h"
 #include "Add.h"
 
-void setUp(void)
-{
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test_Add_given_2_and_3_expect_5() {
+  int result = add(2, 3);
+  TEST_ASSERT_EQUAL(5, result);
 }
 
-void tearDown(void)
-{
-}
-
-void test_Add_NeedToImplement(void)
-{
-    TEST_IGNORE_MESSAGE("Need to Implement Add");
+void test_Add_given_6_and_7_expect_13() {
+  int result = add(6, 7);
+  TEST_ASSERT_EQUAL(13, result);
 }
